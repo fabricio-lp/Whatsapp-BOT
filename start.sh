4b9845fa59612c007bd1f6e30b3de56b7e9c8176
Fabrício Lisboa <mbr47728@gmail.com>
2026-02-20 09:44:42 -0300

    fix: simplify setup

--- start.sh
+++ start.sh
@@ -1,24 +1,18 @@
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-PLTREMTGRAY='\033[0;37m'
-DARKGRAY='\033[1;30m'
-PURPLE='\033[1;31m'
-PLTREMTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-PLTREMTRED='\033[1;34m'
-PLTREMTPURPLE='\033[1;35m'
-PLTREMTCYAN='\033[1;36m'
-WHITE='\033[1;37m'
-while : 
-do
-echo "INICIANDO..."
-npm i
-node index.js
-sleep 1
-clear
+while :; do
+  clear
+  echo "INICIANDO..."
+
+  echo "Verificando configs..."
+  find . -type f -name "*.example" | while read -r example; do
+    target="${example%.example}"
+    if [ ! -f "$target" ]; then
+      cp "$example" "$target"
+      echo "Criado: $target"
+    fi
+  done
+  echo "Configs OK"
+
+  npm i
+
+  node index.js
 done
